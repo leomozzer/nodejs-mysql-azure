63db56ec2c9c08937dd7a709e8f2eef9963a9c7c
leomozzer <mozzer.ferraz@gmail.com>
2022-04-24 20:28:19 -0300

    Updated tf-backend

--- terraform-backend/main.tf
+++ terraform-backend/main.tf
@@ -6,37 +6,38 @@ resource "random_string" "random" {
 }
 
 locals {
-  project_name = "${var.app_name}-${random_string.random.result}"
+  project_name  = "${var.app_name}-${random_string.random.result}"
+  random_result = random_string.random.result
 }
 
-# module "backend" {
-#   source = "./modules"
-#   rg_name = ""
-#   rg_location = ""
-#   acr_name = ""
-#   acr_sku = ""
-#   admin_enabled = false
-#   storage_account_name = ""
-#   account_tier = "Standard"
-#   account_replication_type = "LRS"
-#   storage_container_name = ""
-#   container_access_type = "private"
+module "backend" {
+  source                   = "./modules"
+  rg_name                  = "${local.project_name}-rg-backend"
+  rg_location              = "West Europe"
+  acr_name                 = "${local.random_result}acr"
+  acr_sku                  = "Basic"
+  acr_admin_enabled        = false
+  storage_account_name     = "${local.random_result}stac"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  storage_container_name   = "terraform"
+  container_access_type    = "private"
+}
+
+
+
+# resource "azurerm_resource_group" "rg" {
+#   name     = "${local.project_name}-rg-backend"
+#   location = "West Europe"
 # }
 
-
-
-resource "azurerm_resource_group" "rg" {
-  name     = "${local.project_name}-rg-backend"
-  location = "West Europe"
-}
-
-resource "azurerm_container_registry" "acr" {
-  name                = "${random_string.random.result}acr"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
-  admin_enabled       = false
-}
+# resource "azurerm_container_registry" "acr" {
+#   name                = "${random_string.random.result}acr"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   sku                 = "Basic"
+#   admin_enabled       = false
+# }
 #https://www.youtube.com/watch?v=7ftpkd2DFJ0
 
 # resource "azurerm_storage_account" "storage_account" {
---
+++ terraform-backend/modules/variables.tf
@@ -0,0 +1,39 @@
+variable "rg_name" {
+  type = string
+}
+
+variable "rg_location" {
+  type = string
+}
+
+variable "acr_name" {
+  type = string
+}
+
+variable "acr_sku" {
+  type = string
+}
+
+variable "acr_admin_enabled" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "account_tier" {
+  type = string
+}
+
+variable "account_replication_type" {
+  type = string
+}
+
+variable "storage_container_name" {
+  type = string
+}
+
+variable "container_access_type" {
+  type = string
+}
