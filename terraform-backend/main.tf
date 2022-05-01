e999c61f0dc6dab150bd953584a2246a2daed5ef
leomozzer <mozzer.ferraz@gmail.com>
2022-05-01 18:13:08 -0300

    Updated pipeline

--- terraform-backend/main.tf
+++ terraform-backend/main.tf
@@ -11,18 +11,18 @@ locals {
 }
 
 module "backend" {
-  source                   = "./modules"
-  rg_name                  = "${local.project_name}-rg-backend"
-  rg_location              = "West Europe"
-  acr_name                 = "${local.random_result}acr"
-  acr_sku                  = "Basic"
-  acr_admin_enabled        = false
-  storage_account_name     = "${local.random_result}stac"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  storage_container_name   = "terraform"
-  container_access_type    = "private"
-  keyvault_name            = "${local.random_result}-kv"
+  source            = "./modules"
+  rg_name           = "${local.project_name}-rg-backend"
+  rg_location       = "West Europe"
+  acr_name          = "${local.random_result}acr"
+  acr_sku           = "Basic"
+  acr_admin_enabled = false
+  # storage_account_name     = "${local.random_result}stac"
+  # account_tier             = "Standard"
+  # account_replication_type = "LRS"
+  # storage_container_name   = "terraform"
+  # container_access_type    = "private"
+  keyvault_name = "${local.random_result}-kv"
 }
 
 output "rg_name" {
--- terraform-backend/modules/main.tf
+++ terraform-backend/modules/main.tf
@@ -18,23 +18,23 @@ resource "azurerm_container_registry" "acr" {
 }
 #https://www.youtube.com/watch?v=7ftpkd2DFJ0
 
-resource "azurerm_storage_account" "storage_account" {
-  name                     = var.storage_account_name #random_string.random.result
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = var.account_tier             #"Standard"
-  account_replication_type = var.account_replication_type #"LRS"
+# resource "azurerm_storage_account" "storage_account" {
+#   name                     = var.storage_account_name #random_string.random.result
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   location                 = azurerm_resource_group.rg.location
+#   account_tier             = var.account_tier             #"Standard"
+#   account_replication_type = var.account_replication_type #"LRS"
 
-  # tags = {
-  #   environment = "staging"
-  # }
-}
+#   # tags = {
+#   #   environment = "staging"
+#   # }
+# }
 
-resource "azurerm_storage_container" "storage_container" {
-  name                  = var.storage_container_name
-  storage_account_name  = azurerm_storage_account.storage_account.name
-  container_access_type = var.container_access_type #"private"
-}
+# resource "azurerm_storage_container" "storage_container" {
+#   name                  = var.storage_container_name
+#   storage_account_name  = azurerm_storage_account.storage_account.name
+#   container_access_type = var.container_access_type #"private"
+# }
 
 resource "azurerm_key_vault" "keyvault" {
   name                        = var.keyvault_name
