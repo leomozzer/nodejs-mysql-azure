484ebc565a9778dc45afdcb0621e719b494b44d4
leomozzer <mozzer.ferraz@gmail.com>
2022-05-01 18:24:05 -0300

    Updated pipeline

--- terraform-backend/modules/variables.tf
+++ terraform-backend/modules/variables.tf
@@ -18,25 +18,25 @@ variable "acr_admin_enabled" {
   type = string
 }
 
-variable "storage_account_name" {
-  type = string
-}
+# variable "storage_account_name" {
+#   type = string
+# }
 
-variable "account_tier" {
-  type = string
-}
+# variable "account_tier" {
+#   type = string
+# }
 
-variable "account_replication_type" {
-  type = string
-}
+# variable "account_replication_type" {
+#   type = string
+# }
 
-variable "storage_container_name" {
-  type = string
-}
+# variable "storage_container_name" {
+#   type = string
+# }
 
-variable "container_access_type" {
-  type = string
-}
+# variable "container_access_type" {
+#   type = string
+# }
 
 variable "keyvault_name" {
   type = string
