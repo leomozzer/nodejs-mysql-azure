39406ee8dd93c1420e460f470c99949f7f451d84
leomozzer <mozzer.ferraz@gmail.com>
2022-05-02 22:16:23 -0300

    Fix db issues

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,5 +1,5 @@
 const routes = require('express').Router();
-const { initDatabase, getSubscribers, addSubscriber } = require('./controllers/controllers')
+//const { initDatabase, getSubscribers, addSubscriber } = require('./controllers/controllers')
 
 routes.get('/', (req, res) => {
     return res.send(`Hello World! ${new Date()}`)
